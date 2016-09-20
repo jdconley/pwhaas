db3f90e4b90889dd3143114672e725fc57fe0cdd
JD Conley <jd.conley@gmailcom>
2016-09-19 22:32:04 -0700

    Measuring time

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@
 import * as express from "express";
 import * as argon2 from "argon2";
 import * as timing from "../timing";
+import {Stopwatch} from "../stopwatch";
 
 module Route {
     export class Index {
@@ -11,33 +12,39 @@ module Route {
             res.render("index");
         }
 
-        public hash(req: express.Request, res: express.Response, next: express.NextFunction) {
+        public async hash(req: express.Request, res: express.Response, next: express.NextFunction):Promise<any> {
             //TODO: precondition checking
             //TODO: error handling
             //TODO: options support
-            //TODO: timings / rate limits
+            //TODO: queueing based on capacity
             
             const options = argon2.defaults;
             const plain = req.param("plain");
+            const maxtime = Number(req.param("maxtime") || 500);
 
-            argon2.generateSalt(32).then(salt => {
-                argon2.hash(plain, salt, options).then(hash => {
-                    res.json({hash, options, timing: new timing.Timing()});
-                });
-            });
+            const sw = new Stopwatch();
+            const salt = await argon2.generateSalt(32);
+            const saltTiming = sw.reset(true);
+
+            const hash = await argon2.hash(plain, salt, options);
+            const hashTiming = sw.stop();
+
+            res.json({hash, options, timing: {salt: saltTiming, hash: hashTiming}});
         }
 
-        public verify(req: express.Request, res: express.Response, next: express.NextFunction) {
+        public async verify(req: express.Request, res: express.Response, next: express.NextFunction):Promise<any> {
             //TODO: precondition checking
             //TODO: error handling
-            //TODO: timings / rate limits
+            //TODO: queueing based on capacity
 
             const hash = req.param("hash");
             const plain = req.param("plain");
 
-            argon2.verify(hash, plain).then(match => {
-                res.json({match, timing: new timing.Timing()});
-            });
+            const sw = new Stopwatch();
+            const match = await argon2.verify(hash, plain);
+            const hashTiming = sw.stop();
+            
+            res.json({match, timing: {hash: hashTiming}});
         }
     }
 }
